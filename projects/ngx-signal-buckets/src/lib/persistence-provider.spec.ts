8bc2b863a2c9330223229f760a0e991e38b5032c
Ab Reitsma <ab.reitsma@politie.nl>
2024-02-13 08:24:11 +0100

    added localStorage and sessionStorage unittests

--- projects/ngx-signal-buckets/src/lib/persistence-provider.spec.ts
+++ projects/ngx-signal-buckets/src/lib/persistence-provider.spec.ts
@@ -3,27 +3,94 @@ import { TestBed } from '@angular/core/testing';
 
 import { LocalStoragePersistence, ServerPersistence, SessionStoragePersistence } from './persistence-provider';
 
-describe('LocalStoragePersistence', () => {
-  beforeEach(() => {
-    localStorage.clear();
-    TestBed.configureTestingModule({});
-  });
+describe('Local/SessionStoragePersistence', () => {
+  [
+    { persistence: LocalStoragePersistence, storage: localStorage },
+    { persistence: SessionStoragePersistence, storage: sessionStorage }
+  ].forEach(testElement => {
+    const storage = testElement.storage;
+    const provider = testElement.persistence;
 
-  it('should be created', () => {
-    const service = TestBed.inject(LocalStoragePersistence);
-    expect(service).toBeTruthy();
-  });
-});
+    describe(provider.name, () => {
+      beforeEach(() => {
+        storage.clear();
+        TestBed.configureTestingModule({});
+      });
 
-describe('SessionStoragePersistence', () => {
-  beforeEach(() => {
-    localStorage.clear();
-    TestBed.configureTestingModule({});
-  });
+      it('should be created', () => {
+        const service = TestBed.inject(provider);
+        expect(service).toBeTruthy();
+      });
 
-  it('should be created', () => {
-    const service = TestBed.inject(SessionStoragePersistence);
-    expect(service).toBeTruthy();
+      describe('initialize(lookupIds: Iterable<string>): Observable<SerializedSignal>', () => {
+        it('should complete the returned observable', (done) => {
+          const service = TestBed.inject(provider);
+          expect(service).toBeTruthy();
+          service.initialize([]).subscribe({
+            complete: () => {
+              done();
+            }
+          });
+        });
+
+        it('should return the serialized values of the provided lookupIds that are persisted', (done) => {
+          storage.setItem('id1', '"value1"');
+          storage.setItem('id2', '"value2"');
+          const service = TestBed.inject(provider);
+          const receivedIds: string[] = [];
+          service.initialize(['id1', 'id2']).subscribe({
+            next: serializedSignal => receivedIds.push(serializedSignal.id),
+            complete: () => {
+              expect(receivedIds).toContain('id1');
+              expect(receivedIds).toContain('id1');
+              done();
+            }
+          });
+        });
+
+        it('should not return the serialized values of the provided lookupIds that are not persisted', (done) => {
+          storage.setItem('id1', '"value1"');
+          storage.setItem('id2', '"value2"');
+          const service = TestBed.inject(provider);
+          const receivedIds: string[] = [];
+          service.initialize(['id2', 'id3']).subscribe({
+            next: serializedSignal => receivedIds.push(serializedSignal.id),
+            complete: () => {
+              expect(receivedIds).not.toContain('id3');
+              done();
+            }
+          });
+        });
+
+        it('should not return the serialized values of lookupIds that were not requested', (done) => {
+          storage.setItem('id1', '"value1"');
+          storage.setItem('id2', '"value2"');
+          const service = TestBed.inject(provider);
+          const receivedIds: string[] = [];
+          service.initialize(['id2', 'id3']).subscribe({
+            next: serializedSignal => receivedIds.push(serializedSignal.id),
+            complete: () => {
+              expect(receivedIds).not.toContain('id1');
+              done();
+            }
+          });
+        });
+      });
+
+      describe('persistValue(value: SerializedSignal)', () => {
+        // this persistence provider works synchronous, so no need to return an observable
+        it('should not return a value', () => {
+          const service = TestBed.inject(provider);
+          expect(service.persistValue({ id: 'id1', serializedValue: '1' })).toBeUndefined();
+        });
+
+        it('should persist the serialized value', () => {
+          const service = TestBed.inject(provider);
+          service.persistValue({ id: 'id1', serializedValue: '1' });
+          expect(storage.getItem('id1')).toBe('1');
+        });
+      });
+    });
   });
 });
 
